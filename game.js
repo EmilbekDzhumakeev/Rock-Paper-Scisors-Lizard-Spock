873c240cf33ccb72f1acb4022430590cdf1da0c8
EmilbekDzhumakeev <emilbek.dzhumakeev17@gmail.com>
2021-05-25 21:15:24 -0400

    created child class Ai and Human, made array of gestures in  Player class, defined displayRules() method

--- game.js
+++ game.js
@@ -12,7 +12,11 @@ class Game {
       
     }
    ////////////////////////////////////////////////////////////////////////////
-    displayRules() {
+    displayRules() { 
+      console.log("Welcome to the R-P-S-L-R Game!")
+      console.log("Two players will choose from array of getures")
+      console.log("Each gesture can beat or be beaten by two gestures");
+      console.log("The first player to win three rounds will win the game!");
     
     }
    ////////////////////////////////////////////////////////////////////////////
--- player.js
+++ player.js
@@ -3,10 +3,15 @@ class Player {
     constructor(name) {
       this.score = 0;
       this.name = name; 
-      this. gesture = []
+      this. gesture = [Rock, Paper, Scisors, Lizard, Spock]
     }
   
     
+  } 
+  class Ai extends Player{
+      super(name)
   }
-
+  class Human extends Player{
+    super(name)
+}
   module.exports = Player
